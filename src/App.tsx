afe9067c2f9e8ee4dd2d0f1ad8fcdcc004d562c9
Illia Rozhok <i_rozhok@icloud.com>
2025-09-12 14:47:26 +0300

    remove unused sections

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-import PortfolioGallery from "./components/PortfolioGallery";
 
 function App() {
   return (
